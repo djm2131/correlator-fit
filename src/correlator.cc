c5fbdfd6fe07949f5c4e911d7356655c757d595b
David Murphy <david.murphy@ligo.org>
2017-03-20 19:37:46 -0400

    Add fit function for extracting Epipi - 2*mpi from ratio method

--- src/correlator.cc
+++ src/correlator.cc
@@ -120,15 +120,16 @@ Correlator::Correlator(fitter_controls& fc, int this_fit)
   // The available forms are defined in include/fit_functions.h
   // double V = static_cast<double>( pow(fc.L,3)*fc.T );
   double V = static_cast<double>( pow(fc.L,3) );
-  if     (fc.fits[this_fit].fit_type == "constant"     ){ f = new ConstFunc();                              }
-  else if(fc.fits[this_fit].fit_type == "linear"       ){ f = new LinearFunc();                             }
-  else if(fc.fits[this_fit].fit_type == "exp"          ){ f = new ExpFunc(V);                               }
-  else if(fc.fits[this_fit].fit_type == "double_exp"   ){ f = new DoubleExpFunc(V);                         }
-  else if(fc.fits[this_fit].fit_type == "cosh_pp"      ){ f = new CoshFuncPP(fc.T, V);                      } 
-  else if(fc.fits[this_fit].fit_type == "cosh_ap"      ){ f = new CoshFuncAP(fc.T, V);                      }
-  else if(fc.fits[this_fit].fit_type == "cosh_decay"   ){ f = new CoshFuncDecay(fc.T, V);                   }
-  else if(fc.fits[this_fit].fit_type == "cosh_two_pion"){ f = new CoshFuncTwoPion(fc.T,V);                  }
-  else if(fc.fits[this_fit].fit_type == "zv"           ){ f = new ZVFunc(fc.T, V, fc.fits[this_fit].t_sep); }
+  if     (fc.fits[this_fit].fit_type == "constant"      ){ f = new ConstFunc();                              }
+  else if(fc.fits[this_fit].fit_type == "linear"        ){ f = new LinearFunc();                             }
+  else if(fc.fits[this_fit].fit_type == "exp"           ){ f = new ExpFunc(V);                               }
+  else if(fc.fits[this_fit].fit_type == "double_exp"    ){ f = new DoubleExpFunc(V);                         }
+  else if(fc.fits[this_fit].fit_type == "cosh_pp"       ){ f = new CoshFuncPP(fc.T, V);                      } 
+  else if(fc.fits[this_fit].fit_type == "cosh_ap"       ){ f = new CoshFuncAP(fc.T, V);                      }
+  else if(fc.fits[this_fit].fit_type == "cosh_decay"    ){ f = new CoshFuncDecay(fc.T, V);                   }
+  else if(fc.fits[this_fit].fit_type == "cosh_two_pion" ){ f = new CoshFuncTwoPion(fc.T,V);                  }
+  else if(fc.fits[this_fit].fit_type == "ratio_two_pion"){ f = new TwoPionRatioFunc(fc.T,V);                 }
+  else if(fc.fits[this_fit].fit_type == "zv"            ){ f = new ZVFunc(fc.T, V, fc.fits[this_fit].t_sep); }
   else{ 
     printf("Error: unrecognized fit type %s\n", fc.fits[this_fit].fit_type.c_str()); 
     exit(-1); 
